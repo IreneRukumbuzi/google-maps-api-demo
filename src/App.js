8e79ccf1f8394cc9ee9885db550fe77dce1f520c
Stizo <rukkirene21@gmail.com>
2024-05-02 18:21:39 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, DirectionsRenderer, DirectionsService } from '@react-google-maps/api';
+import { MenuOutlined, HeartOutlined, ExclamationCircleOutlined, BellOutlined } from '@ant-design/icons';
 import CurrentLocation from './CurrentLocation';
 import './App.css';
 
 const containerStyle = {
   width: '100vw',
-  height: 'calc(100vh - 140px)'
+  height: '71vh'
 };
 
 const center = {
@@ -27,7 +28,7 @@ const waypoints = [
 ];
 
 const App = () => {
- 
+
   const [directionsResponse, setDirectionsResponse] = useState(null);
   const [startingPointName, setStartingPointName] = useState(null);
   const [endingPointName, setEndingPointName] = useState(null);
@@ -38,12 +39,12 @@ const App = () => {
     if (directionsResponse && directionsResponse.routes[0]?.legs?.length > 0) {
       const startAddress = directionsResponse.routes[0].legs[0]?.start_address;
       setStartingPointName(startAddress);
-  
+
       const endAddress = directionsResponse.routes[0].legs[directionsResponse.routes[0].legs.length - 1]?.end_address;
       setEndingPointName(endAddress);
     }
-  }, [directionsResponse]);  
-  
+  }, [directionsResponse]);
+
 
   useEffect(() => {
     if (directionsResponse && nextStopIndex < waypoints?.length - 4) {
@@ -52,7 +53,7 @@ const App = () => {
       setNextStopIndex(nextStopIndex);
     }
   }, [directionsResponse, nextStopIndex]);
-  
+
   const fetchNextStopName = async (location) => {
     const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=${process.env.REACT_APP_API_KEY}`);
     const data = await response.json();
@@ -61,63 +62,72 @@ const App = () => {
       setNextStopName(formattedAddress);
     }
   };
-  
+
 
   return (
-    <>
-      <div className="gradient-container">
-        <div className="top-section">
-          <div className="menu-icon">&#9776;</div>
-          <div className="startup-text">Startup</div>
-        </div>
-        <div className="bottom-section">
-          <div className="bottom-text">
-            {startingPointName && endingPointName && <div className="bold">{startingPointName} - {endingPointName}</div>}
-            {nextStopName && <div className="not-bold">Next Stop: {nextStopName}</div>}
-            {directionsResponse && 
-              <div className="not-bold">
-                Distance: {(directionsResponse.routes[0]?.legs.reduce((total, leg) => total + leg.distance.value, 0) / 1000).toFixed(2)} km  
-                Time: {Math.round(directionsResponse.routes[0]?.legs.reduce((total, leg) => total + leg.duration.value, 0) / 60)} minutes
-              </div>
-            }
+    <div className="container">
+      <div className="header">
+        <div className="gradient-container">
+          <div className="top-section">
+            <MenuOutlined className="icon" />
+            <div className="startup-text">Startup</div>
+          </div>
+          <div className="bottom-section">
+            <div className="bottom-text">
+              {startingPointName && endingPointName && <div className="bold">{startingPointName} - {endingPointName}</div>}
+              {nextStopName && <div className="not-bold">Next Stop: {nextStopName}</div>}
+              {directionsResponse &&
+                <div className="not-bold">
+                  Distance: {(directionsResponse.routes[0]?.legs.reduce((total, leg) => total + leg.distance.value, 0) / 1000).toFixed(2)} km
+                  Time: {Math.round(directionsResponse.routes[0]?.legs.reduce((total, leg) => total + leg.duration.value, 0) / 60)} minutes
+                </div>
+              }
+            </div>
           </div>
         </div>
       </div>
-      <LoadScript
-        googleMapsApiKey={process.env.REACT_APP_API_KEY}
-      >
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={20}
-          options={{
-            mapTypeControl: false,
-            streetViewControl: false,
-            fullscreenControl: false,
-            zoomControl: false,
-            compassControl: true
-          }}
+      <div className="map-container">
+        <LoadScript
+          googleMapsApiKey={process.env.REACT_APP_API_KEY}
         >
-         <CurrentLocation />
-
-          <DirectionsService
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={20}
             options={{
-              origin: center,
-              destination: destination,
-              waypoints: waypoints.map(waypoint => ({ location: waypoint.location, stopover: true })),
-              travelMode: 'DRIVING'
+              mapTypeControl: false,
+              streetViewControl: false,
+              fullscreenControl: false,
+              zoomControl: false,
+              compassControl: true
             }}
-            callback={(response, status) => {
-              if (status === 'OK' && !directionsResponse) {
-                setDirectionsResponse(response);
-              }
-            }}
-          />
+          >
+            <CurrentLocation />
 
-          {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
-        </GoogleMap>
-      </LoadScript>
-    </>
+            <DirectionsService
+              options={{
+                origin: center,
+                destination: destination,
+                waypoints: waypoints.map(waypoint => ({ location: waypoint.location, stopover: true })),
+                travelMode: 'DRIVING'
+              }}
+              callback={(response, status) => {
+                if (status === 'OK' && !directionsResponse) {
+                  setDirectionsResponse(response);
+                }
+              }}
+            />
+
+            {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
+          </GoogleMap>
+        </LoadScript>
+      </div>
+      <div className="footer">
+        <HeartOutlined className="icon" />
+        <ExclamationCircleOutlined className="icon" />
+        <BellOutlined className="icon" />
+      </div>
+    </div>
   );
 };
 
